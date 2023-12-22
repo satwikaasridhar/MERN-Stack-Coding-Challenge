a6efc01da598ae0d969deb67c24070385bc01983
satwikaasridhar <112646456+satwikaasridhar@users.noreply.github.com>
2023-12-22 23:02:53 +0530

    Create TransactionBarCharts.js

---
+++ components/TransactionBarCharts.js
@@ -0,0 +1,11 @@
+comimport React from 'react';
+
+function TransactionsBarChart({ barChartData }) {
+    return (
+        <div>
+            {/* Display bar chart using barChartData */}
+        </div>
+    );
+}
+
+export default TransactionsBarChart;
